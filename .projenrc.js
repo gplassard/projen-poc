c0b60fd5647a324bd73f7bef8c49bef2f2f63540
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-10-13 19:22:38 +0200

    fix(dependencies): fix  dependencies job (maybe) (#2)
    ci(dependencies): fix  dependencies job (maybe)

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,17 @@
 const { typescript } = require('projen');
+const { GithubCredentials } = require('projen/lib/github');
+
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'projen-poc',
   gitignore: ['.vscode', '.idea', '*.iml'],
   githubOptions: {
     mergify: false,
+    projenCredentials: GithubCredentials.fromPersonalAccessToken({ secret: 'GITHUB_TOKEN' }),
   },
   jestOptions: {
     configFilePath: 'jest.config.json',
   },
-
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
